cbfc0fa4318413e5ba0a3caf6e1ad1b2cb78408a
SVar25 <varakuta3112@gmail.com>
2020-12-03 18:11:46 -0600

    added new element

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -10,15 +10,18 @@ public class SimpleTest {
     @Test
     public void simpleTest() throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "d:/Work/Projects/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Svitlana\\Documents\\GITProject\\ChromDriver\\chromedriver.exe");
 
         WebDriver browser = new ChromeDriver();
         browser.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
         WebElement name = browser.findElement(By.xpath("//strong/a"));
+        WebElement name1 = browser.findElement(By.xpath("//span/a"));
 
         Assert.assertEquals(name.getText(), "PlatformaticaQA");
+        Assert.assertEquals(name1.getText(), "SergeiDemyanenko");
 
         Thread.sleep(3000);
+
         browser.close();
     }
 }
